d23861e51120e0131060f251f7988e01d335cbdc
t-ceccarini <tommy.cecca@gmail.com>
2023-07-10 22:31:53 +0200

    Added folder with evalsets

--- deep_b_spline_approximation/__main__.py
+++ deep_b_spline_approximation/__main__.py
@@ -30,12 +30,31 @@ def download_models(output_dir):
     
     print("Downloaded models")
     
+def download_evalsets(output_dir):
+    print("Downloading models")
+    
+    download_file('https://github.com/t-ceccarini/deep-b-spline-approximation/blob/master/models/ppn_mlp1.pt?raw=true',
+                  os.path.join(output_dir,'ppn_mlp1.pt'))
+    download_file('https://github.com/t-ceccarini/deep-b-spline-approximation/blob/master/models/kpn_mlp4.pt?raw=true',
+                  os.path.join(output_dir,'kpn_mlp4.pt'))
+    
+    download_file('https://github.com/t-ceccarini/deep-b-spline-approximation/blob/master/models/ppn_cnn1.pt?raw=true',
+                  os.path.join(output_dir,'ppn_cnn1.pt'))
+    
+    print("Downloaded models")
 
 def main():
     
     if sys.argv[1] == 'download-models':
         os.makedirs(os.path.join(os.path.dirname(os.path.realpath(__file__)),'models'))
         output_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)),'models')
+        download_models(output_dir)
+        
+        return
+    
+    if sys.argv[1] == 'download-evalsets':
+        os.makedirs(os.path.join(os.path.dirname(os.path.realpath(__file__)),'evalsets'))
+        output_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)),'evalsets')
         download_models(output_dir)
         
         return
